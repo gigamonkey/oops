f8ce094749dd6ad4a2f34e5f9adc33f0d208cf7e
Peter Seibel <peter@gigamonkeys.com>
2017-10-22 15:35:57 -0700

    Dispatching on type

--- lambda/oops.py
+++ lambda/oops.py
@@ -2,13 +2,43 @@
 # Lambda functions for the Oops bot.
 #
 
+import boto3
 import json
+import time
+
+client = boto3.client('lambda')
 
 def oops(event, context):
 
-    body = event
+    "Main entry point for the OopsBot. Dispatches events asynchronously to another Lambda."
 
-    if 'challenge' in body:
-        return body['challenge']
+    type = event['type']
+
+    if type == 'url_verification':
+        # Slack tests a bot when you register the URL by sending a
+        # challenge that you need to echo back.
+        return event['challenge']
+    elif type == 'event_callback':
+        # Otherwise we're actually being invoked. Since we need to
+        # return 200 quickly we actually dispatch asynchronously to
+        # another Lambda function.
+        response = client.invoke(
+            FunctionName='message_received',
+            InvocationType='Event',
+            LogType='None',
+            ClientContext='string',
+            Payload=json.dumps(event).encode('utf-8')
+        )
+        # TODO: should really check for response['status'] == 202
+        # before we return 200.
+        print(response)
+        return 'ok'
     else:
-        return 'hello'
+        return "Don't know what to do with {}".format(type)
+
+
+def message_received(event, context):
+    print("message_received invoked")
+    time.sleep(10)
+    print("Done sleeping")
+    print(json.dumps(event))
