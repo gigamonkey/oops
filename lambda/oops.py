ee86d18f6ca39e4d24b6d0ea29fb2356029b79af
Peter Seibel <peter@gigamonkeys.com>
2017-10-21 19:23:55 -0700

    Cleaning up oop function.

--- lambda/oops.py
+++ lambda/oops.py
@@ -5,16 +5,10 @@
 import json
 
 def oops(event, context):
-    body = json.loads(event['body'])
+
+    body = event
 
     if 'challenge' in body:
-        return respond(body['challenge'])
+        return body['challenge']
     else:
-        return respond('hello')
-
-def respond(body):
-    return {
-        "statusCode": 200,
-        "headers": {},
-        "body": body
-    }
+        return 'hello'
